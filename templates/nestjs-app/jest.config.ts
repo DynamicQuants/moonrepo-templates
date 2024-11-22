8e69550e82ce38fd292152bc1d61ed5264cfbd48
Carlos Cosming <carlos@kingly.cl>
2024-11-21 23:52:19 -0300

    ✨ feat: add jest/swc for testing in nestjs-app

--- templates/nestjs-app/jest.config.ts
+++ templates/nestjs-app/jest.config.ts
@@ -1,4 +1,9 @@
 import config from '@dynamic-quants/config-tools/jest/nestjs';
 import { Config } from 'jest';
 
-export default config as Config;
+export default {
+  ...config,
+  transform: {
+    '^.+\\.(t|j)s?$': '@swc/jest',
+  },
+} as Config;
